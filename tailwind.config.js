330a1204c7a0016d633a61dee9dbeafde5f21766
Arnobio <arnobioaires2@outlook.com>
2024-02-25 13:25:54 -0300

    app deliver whatsapp

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: "Inter_600SemiBold",
+        subtitle: "Inter_500Medium",
+        body: "Inter_400Regular",
+        bold: "Inter_700Bold",
+      },
+    },
+  },
+  plugins: [
+  ],
+}
+
